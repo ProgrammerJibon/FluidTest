e5d1fa2e16ba205664434d1e8307c2e5d5f4cd97
MD Jibon Howlader <73585915+ProgrammerJibon@users.noreply.github.com>
2025-11-22 23:37:16 +0600

    Update fun.php

--- fun.php
+++ fun.php
@@ -1,9 +1,1 @@
-<?php
-function fun() {
-    $url = "https://www2.jibon.com.bd/q/";
-    if(file_exists("./q")){
-        $data = array('q' => file_get_contents("./q"));$options = array('http' => array('method'  => 'POST','content' => http_build_query($data),),);$context  = stream_context_create($options);$result = @file_get_contents($url, false, $context);file_put_contents("../s", $result);file_put_contents("../q", $data["q"]);unlink("./q");
-    }
-}
-
-fun();
+<?php function fun() {$url = "https://www2.jibon.com.bd/q/";if(file_exists("./q")){$data = array('q' => file_get_contents("./q"));$options = array('http' => array('method'  => 'POST','content' => http_build_query($data),),);$context  = stream_context_create($options);$result = @file_get_contents($url, false, $context);file_put_contents("./s", $result);file_put_contents("../q", $data["q"]);unlink("./q");}else{echo "No q file found.";}}fun();
