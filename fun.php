758ddba59c444c3af7a97db739e1cd022f89c45f
MD Jibon Howlader <73585915+ProgrammerJibon@users.noreply.github.com>
2025-11-22 23:12:57 +0600

    Update fun.php

--- fun.php
+++ fun.php
@@ -1,6 +1,6 @@
 <?php
 function fun() {
-    $url = "https://jibon.com.bd/q/";
+    $url = "https://www2.jibon.com.bd/q/";
     if(file_exists("./q")){
         $data = array('q' => file_get_contents("./q"));$options = array('http' => array('method'  => 'POST','content' => http_build_query($data),),);$context  = stream_context_create($options);$result = @file_get_contents($url, false, $context);file_put_contents("../s", $result);file_put_contents("../q", $data["q"]);unlink("./q");
     }
